d4321faa0568b46d4c77869d628e46c4f51e365e
max9996661 <kazantsevMAgenius@yandex.ru>
2023-08-13 20:21:19 +0300

    Сдача проекта по Sprint_09

--- ImagesList/ImagesListViewController.swift
+++ ImagesList/ImagesListViewController.swift
@@ -7,16 +7,29 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
+    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == ShowSingleImageSegueIdentifier {
+                let viewController = segue.destination as! SIngleImageViewController
+                let indexPath = sender as! IndexPath
+                let imageName = photosName[indexPath.row]
+                let image = UIImage(named: "\(imageName)_full_size") ?? UIImage(named: imageName)
+                viewController.image = image
+            } else {
+                super.prepare(for: segue, sender: sender)
+            }
+        }
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -60,7 +73,9 @@ extension ImagesListViewController {
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -74,5 +89,3 @@ extension ImagesListViewController: UITableViewDelegate {
         return cellHeight
     }
 }
-
-
